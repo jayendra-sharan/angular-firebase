da7dc0da2a09a8cc70f0a9c5ce1482b794a78834
jayendra-sharan <jayendra.sharan@gmail.com>
2021-04-12 11:50:20 +0530

    Upload profile

--- src/app/user/user-dashboard/user-dashboard.component.ts
+++ src/app/user/user-dashboard/user-dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireStorage, AngularFireUploadTask } from '@angular/fire/storage';
+import { finalize } from 'rxjs/operators';
 import { AuthService } from 'src/app/core/auth.service';
 import { User } from '../user.model';
 import { UserService } from '../user.service';
@@ -11,9 +13,12 @@ import { UserService } from '../user.service';
 export class UserDashboardComponent implements OnInit {
   editing = false;
   user: User;
+  task: AngularFireUploadTask;
+
   constructor(
     private auth: AuthService,
-    private userService: UserService
+    private userService: UserService,
+    private storage: AngularFireStorage
   ) { }
 
   ngOnInit(): void {
@@ -35,5 +40,26 @@ export class UserDashboardComponent implements OnInit {
     return this.userService.updateEmail(
       this.user.email
     )
+  }
+
+  uploadPhotoURL(event):void {
+    const file = event.target.files[0];
+    const path = `users/${this.user.uid}/photos/${file.name}`;
+    if (file.type.split('/')[0] !== 'image') {
+      return alert('Only image allowed.');
+    } else {
+      const task: AngularFireUploadTask = this.storage.upload(path, file)
+      const ref = this.storage.ref(path);
+      const uploadPercent = task.percentageChanges();
+      task.snapshotChanges().pipe(
+        finalize(() => {
+          const downloadURL = ref.getDownloadURL();
+          downloadURL.subscribe(url => {
+            this.userService.updateProfileData(this.user.displayName, url)
+          })
+        })
+      ).subscribe();
+      
+    }
   }
 }
