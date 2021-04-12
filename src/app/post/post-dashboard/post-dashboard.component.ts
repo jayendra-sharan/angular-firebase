d90d2815075dae52c1debd1ef9de3fbe2b51548b
jayendra-sharan <jayendra.sharan@gmail.com>
2021-04-12 17:21:54 +0530

    Blog: Post create

--- src/app/post/post-dashboard/post-dashboard.component.ts
+++ src/app/post/post-dashboard/post-dashboard.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/core/auth.service';
+import { Post } from '../post.model';
+import { PostService } from '../post.service';
 
 @Component({
   selector: 'app-post-dashboard',
@@ -6,10 +10,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-dashboard.component.css']
 })
 export class PostDashboardComponent implements OnInit {
-
-  constructor() { }
+  
+  postForm: FormGroup
+  imageURL: string
+  
+  constructor(
+    private postService: PostService,
+    private fb: FormBuilder,
+    private auth: AuthService
+  ) { }
 
   ngOnInit(): void {
+    this.createForm();
   }
 
+  createForm() {
+    this.postForm = this.fb.group({
+      title: ['', [Validators.minLength(3), Validators.maxLength(40)]],
+      content: [''],
+      draft: false
+    })
+  }
+
+  savePost() {
+    const formData: Post = {
+      author: this.auth.currentUserId,
+      title: this.postForm.get('title').value,
+      image: this.imageURL || null,
+      content: this.postForm.get('content').value,
+      draft: this.postForm.get('draft').value,
+      published: new Date(),
+      claps: 0
+    }
+
+    if (!this.postForm.untouched) {
+      this.postForm.reset();
+      return this.postService.create(formData)
+    }
+  }
 }
--- src/app/post/post.service.ts
+++ src/app/post/post.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Post } from './post.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
 
-  constructor() { }
+  postCollection: AngularFirestoreCollection
+
+  constructor(
+    private afs: AngularFirestore
+  ) {
+    this.postCollection = this.afs.collection("posts", ref => ref.orderBy("claps", "desc").limit(10))
+  }
+
+  create(data: Post) {
+    return this.postCollection.add(data)
+  }
 }
