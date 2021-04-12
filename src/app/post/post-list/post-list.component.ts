550d8502545a499f042c3b91d4937ed9a9906672
jayendra-sharan <jayendra.sharan@gmail.com>
2021-04-12 17:56:00 +0530

    Blog: Post list display

--- src/app/post/post-list/post-list.component.ts
+++ src/app/post/post-list/post-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Post } from '../post.model';
+import { PostService } from '../post.service';
 
 @Component({
   selector: 'app-post-list',
@@ -7,9 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostListComponent implements OnInit {
 
-  constructor() { }
+  posts: Observable<Post[]>
+
+  constructor(
+    private postService: PostService
+  ) { }
 
   ngOnInit(): void {
+    this.posts = this.postService.getPosts();
   }
 
 }
