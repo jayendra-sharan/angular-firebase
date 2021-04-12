203b3e4a0df9d3ab18e79b63913cdbc74ff6b69b
jayendra-sharan <jayendra.sharan@gmail.com>
2021-04-12 17:41:42 +0530

    Blog: Post image upload complete

--- src/app/post/post-dashboard/post-dashboard.component.ts
+++ src/app/post/post-dashboard/post-dashboard.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireStorage, AngularFireUploadTask } from '@angular/fire/storage';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { finalize } from 'rxjs/operators';
 import { AuthService } from 'src/app/core/auth.service';
 import { Post } from '../post.model';
 import { PostService } from '../post.service';
@@ -12,12 +15,15 @@ import { PostService } from '../post.service';
 export class PostDashboardComponent implements OnInit {
   
   postForm: FormGroup
+  uploadPercent: Observable<number>
+  downloadURL: Observable<string>
   imageURL: string
   
   constructor(
     private postService: PostService,
     private fb: FormBuilder,
-    private auth: AuthService
+    private auth: AuthService,
+    private storage: AngularFireStorage
   ) { }
 
   ngOnInit(): void {
@@ -45,7 +51,28 @@ export class PostDashboardComponent implements OnInit {
 
     if (!this.postForm.untouched) {
       this.postForm.reset();
+      this.imageURL = '';
       return this.postService.create(formData)
+    }
+  }
+
+  uploadPostImage(event: any) {
+    const file = event.target.files[0];
+    const path = `posts/${file.name}`;
+    if (file.type.split('/')[0] !== "image") {
+      return alert('Only images allowed.');
+    } else {
+      const task: AngularFireUploadTask = this.storage.upload(path, file);
+      const ref = this.storage.ref(path);
+      this.uploadPercent = task.percentageChanges();
+      task.snapshotChanges().pipe(
+        finalize(() => {
+          this.downloadURL = ref.getDownloadURL();
+          this.downloadURL.subscribe(url => {
+            this.imageURL = url;
+          })
+        })
+      ).subscribe();
     }
   }
 }
