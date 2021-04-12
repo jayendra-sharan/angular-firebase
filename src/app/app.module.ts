3963953273f23af5442665b1ad75cbb3a64ac31d
jayendra-sharan <jayendra.sharan@gmail.com>
2021-04-12 12:22:21 +0530

    Blog: module setup done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -8,17 +9,20 @@ import { CoreModule } from './core/core.module';
 import { RoutingModule } from './routing.module';
 import { AngularFireModule } from "@angular/fire"
 import { environment } from 'src/environments/environment';
+import { PostModule } from './post/post.module';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     MaterialModule,
     AngularFireModule.initializeApp(environment.firebase),
     CoreModule,
-    RoutingModule
+    RoutingModule,
+    PostModule
   ],
   providers: [],
   bootstrap: [AppComponent]
