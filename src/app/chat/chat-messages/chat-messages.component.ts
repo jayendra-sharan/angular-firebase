d8ee43d5d23c92a29d3ac3c5d6aba607f589cde0
jayendra-sharan <jayendra.sharan@gmail.com>
2021-04-26 20:17:57 +0530

    Chat: send and list messages

--- src/app/chat/chat-messages/chat-messages.component.ts
+++ src/app/chat/chat-messages/chat-messages.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Message } from '../message.model';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-chat-messages',
@@ -7,9 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatMessagesComponent implements OnInit {
 
-  constructor() { }
+  messages: Observable<Message[]>
+
+  constructor(
+    private messageService: MessageService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.getMessages();
+  }
+
+  getMessages() {
+    const channelId = this.route.snapshot.paramMap.get('id');
+    this.messages = this.messageService.getMessages(channelId);
   }
 
 }
