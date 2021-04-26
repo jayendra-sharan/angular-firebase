8bc35ab4fe1fc3b1d716b0b03f4945e5ed264481
jayendra-sharan <jayendra.sharan@gmail.com>
2021-04-26 19:03:18 +0530

    Chat: module setup

---
+++ src/app/chat/chat-detail/chat-detail.component.ts
@@ -0,0 +1,31 @@
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  AfterViewChecked
+} from '@angular/core';
+
+@Component({
+  selector: 'app-chat-detail',
+  templateUrl: './chat-detail.component.html',
+  styleUrls: ['./chat-detail.component.css']
+})
+export class ChatDetailComponent implements OnInit {
+  
+  @ViewChild('scroller') private feed: ElementRef;
+  
+  constructor(public el: ElementRef) { }
+
+  ngOnInit(): void {
+  }
+
+  ngAfterViewChecked() {
+    this.scrollToBottom();
+  }
+
+  scrollToBottom(): void {
+    const scrollPane: any = this.el.nativeElement.querySelector('.chat-feed');
+    scrollPane.scrollTop = scrollPane.scrollHeight;
+  }
+}
