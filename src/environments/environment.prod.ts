5e36f18ebf42bde28fed2493af3085478396edbf
jayendra-sharan <jayendra.sharan@gmail.com>
2021-05-03 12:26:43 +0530

    deployment steps

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDdi6MaPQnf8PDXIHIpJI9n6TFE7fx_nDc",
+    authDomain: "angular-fire-f26a7.firebaseapp.com",
+    projectId: "angular-fire-f26a7",
+    storageBucket: "angular-fire-f26a7.appspot.com",
+    messagingSenderId: "48120745707",
+    appId: "1:48120745707:web:d769f4bd68576d6f82f8ae"
+  }
 };
